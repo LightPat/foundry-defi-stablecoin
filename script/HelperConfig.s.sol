f43898b2c4aa24ff344ee3e98b1f9d2fa70fd6e5
Patrick Seeman <patseem189@gmail.com>
2026-02-08 15:53:57 -0500

    finish deployment script and helper config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -7,26 +7,28 @@ import {Script} from "forge-std/Script.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+
     struct NetworkConfig {
-        address wEthUsdPriceFeed;
-        address wBtcUsdPriceFeed;
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
         address weth;
         address wbtc;
         uint256 deployerKey;
     }
 
-    NetworkConfig public activeNetworkConfig;
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
-    constructor() {}
-
-    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
-        return NetworkConfig({
-            wEthUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
-            wBtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
-            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            wbtc: 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0,
-            deployerKey: vm.envUint("PRIVATE_KEY")
-        });
+    constructor() {
+        if (block.chainid == 11_155_111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
     }
 
     function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
