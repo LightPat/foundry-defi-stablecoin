6378b4f900936cd5122ac569ba22e0633309ddb3
Patrick Seeman <patseem189@gmail.com>
2026-02-08 13:32:07 -0500

    DSC engine deposit collateral functionality

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -8,11 +8,10 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 /**
  * @title DecentralizedStableCoin
  * @author Patrick Seeman
- * @notice
  * Collateral: Exogenous (ETH & BTC)
  * Minting: Algorithmic
  * Relative Stability: Pegged to USD
- * 
+ *
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
